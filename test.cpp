1c19abcd10b1c51e31f826de07ed40133c1f8f2f
Fenix <liuruoze@163.com>
2014-09-29 17:22:44 +0800

    add getPlateColor and changParams method
    change the class hierarchy

--- test.cpp
+++ test.cpp
@@ -16,6 +16,15 @@ int main()
 	pr.LoadSVM("model/svm.xml");
 	pr.LoadANN("model/ann.xml");
 
+	pr.setGaussianBlurSize(5);
+	pr.setMorphSizeWidth(17);
+
+	pr.setVerifyMin(3);
+	pr.setVerifyMax(20);
+
+	pr.setLiuDingSize(7);
+	pr.setColorThreshold(150);
+
 	vector<string> plateVec;
 
 	int result = pr.plateRecognize(src, plateVec);
